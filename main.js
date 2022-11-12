d0974125973181be3e015708fb43e28a542e4387
dragvn <liminalinquiries@gmail.com>
2022-11-11 20:40:35 -0500

    -setup files -multiplication table -'neither yes no'

---
+++ main.js
@@ -0,0 +1,29 @@
+// Multiplication table
+// Write a program that asks the user for a number, 
+// then shows the multiplication table for this number.
+// When you are done, improve the program so it only accepts numbers between 2 and 9 
+// (use the previous exercise as a blueprint).
+    // let userNum = 0
+    // let multiTable = []
+
+    // while(userNum <= 2 || userNum > 9){
+    //     userNum = prompt('Please enter a number between 2 and 9:')
+    // }
+    // if(userNum > 2 && userNum <= 9){
+    //     for(i=1; i <= 12; i++){
+    //         multiTable[i]= userNum * i
+    //     }
+    //     console.log(multiTable)
+    // }
+        
+
+// Neither yes nor no 
+// Write a program that plays "neither yes, nor no" with the user. 
+// Specifically, the programs asks the user to enter text 
+// until either "yes" or "no" is typed, which ends the game.
+
+    // let userInput = ''
+    // while(userInput !== 'yes' && userInput !== 'no'){
+    //     userInput = prompt('Please enter yes or no:')
+    // }
+    
