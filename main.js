5a48f9a727ba212387b65776504a5c6c7367e6dc
dragvn <liminalinquiries@gmail.com>
2022-11-12 17:03:01 -0500

    -jsway functions -improved hello -number squaring

--- main.js
+++ main.js
@@ -26,4 +26,44 @@
     // while(userInput !== 'yes' && userInput !== 'no'){
     //     userInput = prompt('Please enter yes or no:')
     // }
-    
+
+
+// Improved hello
+// Complete the following program so that it asks the user for his first and last names, then show the result of the sayHello() function.
+
+// // Say hello to the user
+    // function sayHello() {
+    //     let firstName = prompt('What is your first name?')
+    //     let lastName = prompt('What is your last name?')
+    //     const message = `Hello, ${firstName} ${lastName}!`;
+    //     console.log(message)
+    // }
+    // sayHello()
+// // TODO: ask user for first and last name
+// // TODO: call sayHello() and show its result
+
+
+// Number squaring
+// Complete the following program so that the square1() and square2() functions work properly.
+
+// // // Square the given number x
+//     function square1(x) {
+//         return x * x
+//     }
+
+// // // Square the given number x
+//     const squareTo10 = () => {
+//         for(i=0; i<=10; i++){
+//             console.log(i * i)
+//         }
+//     }
+
+//     console.log(square1(0)); // Must show 0
+//     console.log(square1(2)); // Must show 4
+//     console.log(square1(5)); // Must show 25
+
+//     console.log(squareTo10())
+// // When it's done, update the program so that it shows the square of every number between 0 and 10.
+
+// // Writing 10 dumb calls to square() is forbidden! You know how to repeat statements, don't you? ;)
+
