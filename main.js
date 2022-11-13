c65ae317062142821acf454b39ce51803d768f54
dragvn <liminalinquiries@gmail.com>
2022-11-12 19:43:06 -0500

    -calculator -circle circumfrence/area

--- main.js
+++ main.js
@@ -67,3 +67,70 @@
 
 // // Writing 10 dumb calls to square() is forbidden! You know how to repeat statements, don't you? ;)
 
+// Minimum of two numbers
+// Let's pretend the JavaScript Math.min() function doesn't exist. Complete the following program so that 
+// the min() function returns the minimum of its two received numbers.
+
+// // // TODO: write the min() function
+//     const minOfTwo = (num1, num2) => {
+//         if(num1 > num2){
+//             return num2
+//         }else if(num1 < num2){
+//             return num1
+//         }else{
+//             return num1
+//         }
+//     }
+// console.log(minOfTwo(4.5, 5)); // Must show 4.5
+// console.log(minOfTwo(19, 9));  // Must show 9
+// console.log(minOfTwo(1, 1));   // Must show 1
+
+// Calculator
+// Complete the following program so that it offers the four basic arithmetical operations:
+//  addition, subtraction, multiplication and division. 
+//  You can use either a function declaration or a function expression.
+
+// // TODO: complete program
+// const calculate = (num1, operand, num2) => {
+//     switch(operand) {
+//         case '+':
+//             console.log(num1 + num2);
+//             break;
+//         case '-':
+//             console.log(num1 - num2);
+//             break;
+//         case '*':
+//             console.log(num1 * num2);
+//             break;
+//         case '/':
+//             console.log(num1 / num2);
+//             break;
+//     }
+// }
+// calculate(4, "+", 6);  // Must show 10
+// calculate(4, "-", 6);  // Must show -2
+// calculate(2, "*", 0);  // Must show 0
+// calculate(12, "/", 0); // Must show Infinity
+
+// Circumference and area of a circle
+// Write a program containing two functions to calculate the circumference and area of a circle defined by its radius. 
+// // Test it using user input.
+// let radius = Number(prompt('What is the radius of the circle?'))
+    // const circumference = () => {
+    //     console.log(Math.PI * (2 * radius))
+    // }
+    // const areaOfCircle = () => {
+    //     return (Math.PI * (radius ** 2))
+    // }
+    // circumference()
+    // console.log(areaOfCircle())
+    // console.log(Math.PI)
+    // console.log(radius)
+// // Here are some tips for solving this exercise:
+
+// // Circumference and area calculation formulas should be part of your secondary school memories... Or a Google click away :)
+// // The value of number π (Pi) is obtained with Math.PI in JavaScript.
+// // You might want to use the exponentiation operator ** to perform computations.
+// //console.log(2 ** 3); // 8: 2 * 2 * 2
+// //console.log(3 ** 2); // 9: 3 * 3
+
